fe4579354727821e0ab86529896b66f0aecb150e
ajiedr <ajiedibyor@gmail.com>
2024-11-13 20:01:46 +0700

    Initial Commit

---
+++ gamv/gamvApp.swift
@@ -0,0 +1,17 @@
+//
+//  gamvApp.swift
+//  gamv
+//
+//  Created by Ajie DR on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct gamvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
