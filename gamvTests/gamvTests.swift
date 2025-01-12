b9bab3e9bed15a059f1f863f4a4272322e109528
ajiedr <ajiedibyor@gmail.com>
2025-01-12 08:29:30 +0700

    add dummy unit test

---
+++ gamvTests/gamvTests.swift
@@ -0,0 +1,16 @@
+//
+//  gamvTests.swift
+//  gamvTests
+//
+//  Created by Ajie DR on 12/01/25.
+//
+
+import Testing
+
+struct gamvTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
